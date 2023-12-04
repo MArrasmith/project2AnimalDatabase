5a38df28e7d85c22be170af0a43c462a148c6e7b
ZBurnell <136769106+ZBurnell@users.noreply.github.com>
2023-12-03 21:20:30 -0500

    Added some routes to start with. Need to add more for all functions

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const searchRoutes = require('./searchRoutes');
+const animalRoutes = require('./animalRoutes');
+
+router.use('/users', userRoutes);
+router.use('/search', searchRoutes);
+router.use('/animal', animalRoutes);
 
 module.exports = router;
