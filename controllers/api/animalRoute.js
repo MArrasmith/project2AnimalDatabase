c37a7ea1b256af88afa3bacf68cfd1181093bec6
ZBurnell <136769106+ZBurnell@users.noreply.github.com>
2023-12-04 22:22:17 -0500

    made some changes to the routes

--- controllers/api/animalRoute.js
+++ controllers/api/animalRoute.js
@@ -1,15 +1,15 @@
-const router = require('express').Router();
-const { createOrUpdateAnimal } = require('../../seeds/seeds');
+// const router = require('express').Router();
+// const { createOrUpdateAnimal } = require('../../seeds/seeds');
 
-app.post('/animals/search', async (req, res) => {
-  const { animalName } = req.body;
+// app.post('/animals/search', async (req, res) => {
+//   const { animalName } = req.body;
 
-  try {
-    const animal = await createOrUpdateAnimal(animalName);
-    res.status(200).json({ success: true, animal });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
-});
+//   try {
+//     const animal = await createOrUpdateAnimal(animalName);
+//     res.status(200).json({ success: true, animal });
+//   } catch (error) {
+//     res.status(500).json({ success: false, error: error.message });
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,14 +2,14 @@ const router = require('express').Router()
 const userRoutes = require('./userRoutes');
 const searchRoutes = require('./searchRoutes');
 const animalRoutes = require('./animalRoutes');
-const animalRoutes = require('./animalRoute');
 const funFactRoutes = require('./funFactRoutes');
 
-router.use('/animal', animalRoutes);
-router.use('/fun-fact', funFactRoutes);
+// const animalRoutes = require('./animalRoute');
+// router.use('/animal', animalRoutes);
 
 router.use('/users', userRoutes);
 router.use('/search', searchRoutes);
 router.use('/animal', animalRoutes);
+router.use('/fun-fact', funFactRoutes);
 
 module.exports = router;
